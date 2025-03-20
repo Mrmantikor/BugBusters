b0261120c3bede9f23ea8b1db846db345764be33
lika_vil <125767690+Lika060@users.noreply.github.com>
2025-03-20 14:17:30 +0200

    Changes to be committed: 	deleted:    README.pl.md 	deleted:    README.ro.md 	deleted:    README.tr.md 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/css/animations.css 	deleted:    src/css/back-link.css 	deleted:    src/css/badges.css 	deleted:    src/css/base.css 	new file:   src/css/base/common.css 	new file:   src/css/base/typography.css 	deleted:    src/css/container.css 	deleted:    src/css/footer.css 	deleted:    src/css/header.css 	new file:   src/css/index.css 	new file:   src/css/layouts/01-header.css 	new file:   src/css/layouts/02-hero.css	new file:   src/css/layouts/03-aboutme.css 	new file:   src/css/layouts/04-techskills.css 	new file:   src/css/layouts/05-myproject.css 	new file:   src/css/layouts/06-benefits.css 	new file:   src/css/layouts/07-f-a-q.css 	new file:   src/css/layouts/08-reviews.css 	new file:   src/css/layouts/09-work-together.css 	new file:   src/css/layouts/10-footer.css 	new file:   src/css/layouts/11-modal-window.css 	deleted:    src/css/reset.css 	deleted:    src/css/styles.css 	deleted:    src/css/vite-promo.css 	new file:   src/fonts/impact-regular.woff2 	deleted:    src/img/javascript.svg 	deleted:    src/img/logo.svg 	modified:   src/img/sprite.svg 	deleted:    src/img/vite-logo.png 	deleted:    src/img/webp/vite-logo.webp 	modified:   src/index.html 	new file:   src/js/01-header.js 	new file:   src/js/02-hero.js 	new file:   src/js/03-aboutme.js 	new file:   src/js/04-techskills.js 	new file:   src/js/05-myproject.js 	new file:   src/js/06-benefits.js 	new file:   src/js/07-f-a-q.js 	new file:   src/js/08-reviews.js 	new file:   src/js/09-work-together.js 	new file:   src/js/10-footer.js 	new file:   src/js/11-modal-window.js 	modified:   src/main.js 	deleted:    src/page-2.html 	deleted:    src/page-3.html 	new file:   src/partials/01-header.html 	new file:   src/partials/02-hero.html 	new file:   src/partials/03-aboutme.html 	new file:   src/partials/04-techskills.html 	new file:   src/partials/05-myproject.html 	new file:   src/partials/06-benefits.html 	new file:   src/partials/07-f-a-q.html 	new file:   src/partials/08-reviews.html 	new file:   src/partials/09-work-together.html 	new file:   src/partials/10-footer.html 	new file:   src/partials/11-modal-window.html 	deleted:    src/partials/back-link.html 	deleted:    src/partials/badges.html 	deleted:    src/partials/footer.html 	deleted:    src/partials/header.html 	deleted:    src/partials/vite-promo.html 	modified:   src/public/favicon.svg

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './js/01-header';
+import './js/02-hero';
+import './js/03-aboutme';
+import './js/04-techskills';
+import './js/05-myproject';
+import './js/06-benefits';
+import './js/07-f-a-q';
+import './js/08-reviews';
+import './js/09-work-together';
+import './js/10-footer';
+import './js/11-modal-window';
