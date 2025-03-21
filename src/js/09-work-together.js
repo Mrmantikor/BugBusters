470dbdc3c070fab712a92ab4b5126c8786e6f6b0
demyanyshyn <taras.demyanyshyn@gmail.com>
2025-03-21 18:36:41 +0200

    done 90%

---
+++ src/js/09-work-together.js
@@ -0,0 +1,53 @@
+import iziToast from 'izitoast';
+import axios from 'axios';
+const form = document.querySelector(`#work-together-form`);
+const modalOverlay = document.querySelector(`#work-together-modal-window`);
+console.dir(form);
+const { email, message, button } = form.elements;
+console.dir(email);
+form.addEventListener(`submit`, clickHandler);
+
+function isInputValid(form) {
+  const { email, message } = form.elements;
+  return email.value && message.value ? true : false;
+}
+
+function onSucsessResponse(response) {
+  document.body.classList.add(`modal-open`);
+  modalOverlay.classList.remove(`visually-hidden`);
+}
+
+async function sendQuerry(form) {
+  const { email, message } = form.elements;
+  const querry = {
+    email: email.value,
+    comment: message.value,
+  };
+  console.dir(querry);
+  const url = `https://portfolio-js.b.goit.study/api/requests`;
+  console.log(JSON.stringify(querry));
+  const response = await axios
+    .post(url, querry)
+    .then(response => response.data)
+    .then(response => onSucsessResponse(response))
+    .catch(reject => {
+      console.log(reject);
+      reject.response.data
+        ? iziToast.error(reject)
+        : iziToast.error(reject.response.data);
+    });
+  return response;
+}
+
+async function onSucsessInput(form) {
+  const { email, message } = form.elements;
+  await sendQuerry(form).then(response => {
+    console.log(response);
+  });
+}
+function clickHandler(event) {
+  event.preventDefault();
+  isInputValid(form)
+    ? onSucsessInput(form)
+    : iziToast.warning({ message: `Error` });
+}
